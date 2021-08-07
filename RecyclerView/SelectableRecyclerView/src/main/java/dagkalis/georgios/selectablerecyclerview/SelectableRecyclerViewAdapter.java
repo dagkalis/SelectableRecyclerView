8ceef97ece20fd7c582d6efafd72b468805b3e52
dagkalis <geodagkalis@gmail.com>
2021-08-07 20:46:24 +0300

    new version: 1. setOnNoItemSelectedListener and setOnAtLeastOneItemSelectedListener, 2. built in removeItem so that it is easier to work with callbacks 3. reverseSeletionStatus to select and unselect items easily

--- RecyclerView/SelectableRecyclerView/src/main/java/dagkalis/georgios/selectablerecyclerview/SelectableRecyclerViewAdapter.java
+++ RecyclerView/SelectableRecyclerView/src/main/java/dagkalis/georgios/selectablerecyclerview/SelectableRecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package dagkalis.georgios.selectablerecyclerview;
 
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.view.View;
 
 
 import androidx.annotation.NonNull;
@@ -13,23 +14,38 @@ import java.util.HashSet;
 public abstract class SelectableRecyclerViewAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
 
     private final HashSet<Object> selectedItems = new HashSet<Object>();
-    Drawable selectedItemDrawable;
-    Drawable unSelectedItemDrawable;
-    boolean useDrawables = false;
+    private Drawable selectedItemDrawable;
+    private Drawable unSelectedItemDrawable;
+    private boolean useDrawables = false;
+    private OnNoItemSelectedListener onNoItemSelectedListener;
+    private OnAtLeastOneItemSelectedListener onAtLeastOneItemSelectedListener;
 
     public void selectItem(Object item) {
         boolean result = selectedItems.add(item);
-        if (selectedItems.size() == 1 && result) { // if the size just went from 0 to 1 then fire the event in case the developer wants to know
+        if (selectedItems.size() == 1 && result) { // if the size just went from 0 to 1 then fire the event
             onAtLeastOneItemSelected();
         }
     } //check
 
     public void unSelectItem(Object item) {
         boolean result = selectedItems.remove(item);
-        if (selectedItems.isEmpty() && result) { // if there are no selected items left fire the event in case the developer wants to know
+        if (selectedItems.isEmpty() && result) { // if there are no selected items left fire the event
             onNoItemSelected();
         }
     } // check
+
+    /**
+     * if the item is selected then unselect it
+     * if the item is unselected then select it
+     * @param item
+     */
+    public void reverseSelectionStatus(Object item){
+        if(isSelected(item)){
+            unSelectItem(item);
+        }else{
+            selectItem(item);
+        }
+    }
 
     public void selectItem(int position) {
         selectItem(getItems().get(position));
@@ -38,6 +54,15 @@ public abstract class SelectableRecyclerViewAdapter<VH extends RecyclerView.View
     public void unSelectItem(int position) {
         unSelectItem(getItems().get(position));
     } // check
+
+    /**
+     * if the item in that position is selected then unselect it
+     * if the item in that position is unselected then select it
+     * @param position
+     */
+    public void reverseSelectionStatus(int position){
+        reverseSelectionStatus(getItems().get(position));
+    }
 
     public ArrayList getSelectedItems() {
         ArrayList<Object> selectedItemsArrayList = new ArrayList<Object>(selectedItems.size());
@@ -58,7 +83,7 @@ public abstract class SelectableRecyclerViewAdapter<VH extends RecyclerView.View
     public void unSelectAllItems() {
         boolean onNoItemSelectedFlag = selectedItems.size() > 0;
         selectedItems.clear();
-        if(onNoItemSelectedFlag)
+        if(onNoItemSelectedFlag) // if selected items where more than 0 (and now they are 0 of course)
             onNoItemSelected();
     } //check
 
@@ -73,18 +98,6 @@ public abstract class SelectableRecyclerViewAdapter<VH extends RecyclerView.View
     public int getSelectedItemCount() {
         return selectedItems.size();
     } //check
-
-    /**
-     * Warning. When removing a SELECTED item outside of this libraries methods
-     * then the developer should unSelect the item. Otherwise onNoItemSelected will not work
-     */
-
-    public void onNoItemSelected() {} //check
-
-    /**
-     * Called when selectedItems.size goes from 0 to 1
-     */
-    public void onAtLeastOneItemSelected() {} //check
 
 
     /**
@@ -113,6 +126,29 @@ public abstract class SelectableRecyclerViewAdapter<VH extends RecyclerView.View
         return getItems().size();
     } //check
 
+
+    /**
+     * To remove an item from the ArrayList and also unselect it.
+     * If onNoItemSelected is used then the selectedItems should be updated when removing an item from the arrayList.
+     * This function provides exactly that
+     * @param position in the arrayList
+     */
+    public void removeItem(int position){
+        Object item = getItems().remove(position);
+        unSelectItem(item);
+    }
+
+    /**
+     * To remove an item from the ArrayList and also unselect it.
+     * If onNoItemSelected is used then the selectedItems should be updated when removing an item from the arrayList.
+     * This function provides exactly that
+     * @param item to be removed from the arrayList
+     */
+    public void removeItem(Object item){
+        getItems().remove(item);
+        unSelectItem(item);
+    }
+
     public void removeSelectedItems(){
         getItems().removeAll(selectedItems);
         unSelectAllItems();
@@ -130,13 +166,13 @@ public abstract class SelectableRecyclerViewAdapter<VH extends RecyclerView.View
     public void onBindViewHolder(@NonNull VH holder, int position) {
         if(useDrawables && position < getItems().size()) { // this is only used for custom drawables, check if user has picked drawables and whether the arraylist has a value at this position (only way it is not is if developer has overrided getItemCount
 //            try {
-                boolean isSelected = isSelected(position);
-                if (isSelected) {
-                    holder.itemView.setBackground(selectedItemDrawable);
-                }
-                if (!isSelected) {
-                    holder.itemView.setBackground(unSelectedItemDrawable);
-                }
+            boolean isSelected = isSelected(position);
+            if (isSelected) {
+                holder.itemView.setBackground(selectedItemDrawable);
+            }
+            if (!isSelected) {
+                holder.itemView.setBackground(unSelectedItemDrawable);
+            }
 //            }catch (IndexOutOfBoundsException indexOutOfBoundsException){
 //                System.out.println("position " + position);
 //                System.out.println("itemCount " + getItemCount());
@@ -162,4 +198,49 @@ public abstract class SelectableRecyclerViewAdapter<VH extends RecyclerView.View
         useDrawables = true;
     }
 
+    public interface OnNoItemSelectedListener {
+
+        void onNoItemSelected();
+    }
+
+    /**
+     * Warning. When removing a SELECTED item outside of this libraries methods
+     * then the developer should unSelect the item. Otherwise onNoItemSelected will not work
+     */
+
+    public void onNoItemSelected() {
+        if(onNoItemSelectedListener != null)
+            onNoItemSelectedListener.onNoItemSelected();
+    }
+
+    public void setOnNoItemSelectedListener(OnNoItemSelectedListener l){
+        this.onNoItemSelectedListener = l;
+    }
+
+    public interface OnAtLeastOneItemSelectedListener{
+        void OnAtLeastOneItemSelected();
+    }
+
+    public void setOnAtLeastOneItemSelectedListener(OnAtLeastOneItemSelectedListener l){
+        this.onAtLeastOneItemSelectedListener = l;
+    }
+
+
+    /**
+     * Called when selectedItems.size goes from 0 to 1
+     */
+    public void onAtLeastOneItemSelected() {
+        if(onAtLeastOneItemSelectedListener != null){
+            onAtLeastOneItemSelectedListener.OnAtLeastOneItemSelected();
+        }
+    } //check
+
+
+
+
+
+
 }
+
+
+
